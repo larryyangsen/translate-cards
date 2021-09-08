95be8c61668a432c4665728ea030512fccec1814
larryyangsen <larryyangsen@hotmail.com>
2021-09-08 20:26:57 +0800

    [feat] highlight every words

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useDebouncedCallback } from 'use-debounce';
 import getDicts, { IDictionaryData } from './dictionary';
 import Translate from './Translate';
 
-const words = `Sed ut perspiciatis hello omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`;
+const words = `The Legend of Zelda: Breath of the Wild’s sheer freedom and sense of adventure is a remarkable achievement. Right from the start, the vast landscape of Hyrule is thrown completely open to you, and it constantly finds ways to pique your curiosity with mysterious landmarks, complex hidden puzzles, and enemy camps to raid for treasure and weapons. The fact that you can tackle any one of these things at your own pace and almost never get pulled to the main path is liberating, but the way all of Breath of the Wild’s systems fit elegantly into complex light survival game is even more impressive. I’ve been running around for over 50 hours and I still have plenty of mysteries left to track down and lots of wonderfully crafted puzzles to solve. I’m in awe of the scope and scale of this adventure, and I often find myself counting the hours until I can get back in.`;
 
 const highlightWords = (search: string, words: string[], ref: React.RefObject<HTMLDivElement>) => {
     let hasSearched = false;
@@ -28,14 +28,16 @@ const highlightWords = (search: string, words: string[], ref: React.RefObject<HT
 
 function App() {
     const ref = useRef<HTMLDivElement>(null);
-    const [search, setSearch] = useState('hello');
+    const [search, setSearch] = useState('legend');
     const [leftTop, setLeftTop] = useState({ left: 0, top: 0 });
-    const [[hasSearched,highlightedWords], setHighlightedWords] = useState(highlightWords(search, words.split(' '), ref));
+    const [[hasSearched, highlightedWords], setHighlightedWords] = useState(
+        highlightWords(search, words.split(' '), ref)
+    );
     const [dicts, setDicts] = useState<IDictionaryData[]>([]);
 
     const handleInputChanged = useDebouncedCallback((e: React.FocusEvent<HTMLInputElement>) => {
         setSearch(e.target.value ?? '');
-    }, 200)
+    }, 200);
 
     useEffect(() => {
         const highlighted = highlightWords(search, words.split(' '), ref);
